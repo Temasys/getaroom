ae6543b9d38a08e6d57ccc82724be410db1aae09
Thomas Gorissen <serrynaimo@me.com>
2014-07-15 14:29:45 +0800

    Added semicolon

--- source/jsx/main.jsx
+++ source/jsx/main.jsx
@@ -43,7 +43,7 @@ define([
                         data: false
                     }
                 }
-            }
+            };
         },
         componentWillMount: function() {
            var self = this;
