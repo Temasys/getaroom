1daf00b3dc645fc0509678c7ad12ce06d73539f2
Thomas Gorissen <serrynaimo@me.com>
2014-10-01 11:31:33 +0800

    nonmin

--- source/jsx/loader.jsx
+++ source/jsx/loader.jsx
@@ -16,7 +16,7 @@ require.config({
             'adapter.min',
         //skyway: 'libs/skyway',
         skyway: '//cdn.temasys.com.sg/skyway/skywayjs/' +
-            '0.5.x/skyway.debug.min',
+            '0.5.x/skyway.debug',
         // facebook: '//connect.facebook.net/en_US/all',
         // twitter: '//platform.twitter.com/widgets',
         fastclick: '//cdnjs.cloudflare.com/' +
