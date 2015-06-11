eba83c81e72fc86725910f0eb093b03a89ad8dc0
Thomas Gorissen <serrynaimo@me.com>
2015-06-11 13:19:50 +0800

    using cdn resources

--- source/js/loader.js
+++ source/js/loader.js
@@ -11,12 +11,12 @@ require.config({
     paths: {
         socketio: '//cdn.temasys.com.sg/libraries/'
             + 'socket.io-client/1.0.6/socket.io',
-        adapter: 'libs/adapter',
-        /*adapter: '//cdn.temasys.com.sg/adapterjs/0.10.x/' +
-            'adapter.debug',*/
-        skylink: 'libs/skylink',
-        //skylink: '//cdn.temasys.com.sg/skylink/skylinkjs/0.5.x/' +
-        //    'skylink.debug',
+        //adapter: 'libs/adapter',
+        adapter: '//cdn.temasys.com.sg/adapterjs/0.11.x/' +
+            'adapter.debug',
+        //skylink: 'libs/skylink',
+        skylink: '//cdn.temasys.com.sg/skylink/skylinkjs/dev/0.6.x/' +
+            'skylink.debug',
         // facebook: '//connect.facebook.net/en_US/all',
         // twitter: '//platform.twitter.com/widgets',
         fastclick: '//cdnjs.cloudflare.com/' +
