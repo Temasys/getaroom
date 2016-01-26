3da66cd96ad79979e965a7ef0729310ca4a8bea9
Leticia Choo <leticia.choo@temasys.com.sg>
2016-01-26 15:08:20 +0800

    Accodomate https: to MCU dev ec2 server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -219,8 +219,8 @@ module.exports = function (grunt) {
                 options: {
                     base: '<%= source %>/',
                     port: 8085,
-                    open: 'http://localhost:8085',
-                    protocol: 'http',
+                    open: 'https://localhost:8085',
+                    protocol: 'https',
                     //key: grunt.file.read('server.key').toString(),
                     //cert: grunt.file.read('server.crt').toString(),
                     //ca: grunt.file.read('ca.crt').toString()
